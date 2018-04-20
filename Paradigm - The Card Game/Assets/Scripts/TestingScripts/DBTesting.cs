6184fd6c320952091ceaac42f7bf2502ac69ef3c
Jtayl216 <lorfakl@aol.com>
2018-04-20 10:09:41 -0400

    Made a huge mistake
    After trying to enable XML documentation I really messed up some stuff so Im commiting one line change and rolling the other changes back

--- Paradigm - The Card Game/Assets/Scripts/TestingScripts/DBTesting.cs
+++ Paradigm - The Card Game/Assets/Scripts/TestingScripts/DBTesting.cs
@@ -9,7 +9,7 @@ public class DBTesting : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        CardDataBase.WriteEncodedAbilities();
+        //CardDataBase.WriteEncodedAbilities();
 	}
 	
 	// Update is called once per frame
